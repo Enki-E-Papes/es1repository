43be787afefb42c7256316b897982573e4bcde34
Lich-senpai13 <papesenki.enrico@iisgalvanimi.edu.it>
2023-02-13 19:38:05 +0000

    es1

--- index.py
+++ index.py
@@ -1,8 +1,10 @@
 '''
 Per realizzare un sito web con flask che abbia le seguenti funzionalità :
 1)Home page deve descrivere una breve descrizione delle caratteristeche della città di milano 
-2)Al link /<nomesito>/immagini.deveono essere visualizate le immagini di 4 monumenti di milano(controllare sul sito del prof come mettere le immagini)
-3)fare in modo che cliccando sul immagine si venga mandati ad un breve testo descrittivo del documento(controllare sempre sul sito del docente)
+2)Al link /<nomesito>/immagini.devono essere visualizate le immagini di 4 monumenti di milano(controllare sul sito del
+ prof come mettere le immagini)
+3)fare in modo che cliccando sul immagine si venga mandati ad un breve testo descrittivo del documento(controllare sempre 
+sul sito del docente)
 4)La repository che conterra il sito si chiamerà FlaskMilano
 '''
 
@@ -11,7 +13,23 @@ app = Flask(__name__)   #variabile che identifica il sito web
 
 @app.route('/', methods=['GET'])
 def hello_world():
-    return render_template('index.html')
+    return render_template('index1.html')
+#---------------------------------------------
+@app.route('/img1', methods=['GET'])
+def img1def():
+    return render_template('perImg1.html')
+#---------------------------------------------
+@app.route('/img2', methods=['GET'])
+def img2def():
+    return render_template('perImg2.html')
+#---------------------------------------------  
+@app.route('/img3', methods=['GET'])
+def img3def():
+    return render_template('perImg3.html')
+#---------------------------------------------
+@app.route('/img4', methods=['GET'])
+def img4def():
+    return render_template('perImg4.html')
 
 
 if __name__ == '__main__':
